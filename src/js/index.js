3a2fd8fb864c5bca3e87aa4fb1b23337d0de269c
jessur2030 <jessusdev@gmail.com>
2023-05-15 03:02:25 -0400

    update index js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.querySelector("#nav-menu");
-const navToggle = document.querySelector("#nav-toggle");
-const navClose = document.querySelector("#nav-close");
-const navLink = document.querySelectorAll(".nav__link");
+const navMenu = document.querySelector('#nav-menu');
+const navToggle = document.querySelector('#nav-toggle');
+const navClose = document.querySelector('#nav-close');
+const navLink = document.querySelectorAll('.nav__link');
 
 // test
 //another test
@@ -10,8 +10,8 @@ const navLink = document.querySelectorAll(".nav__link");
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
   });
 }
 
@@ -19,88 +19,88 @@ if (navToggle) {
 /* Validate if constant exists */
 
 if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
   });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const handlerLinkAction = function (handler) {
-  navMenu.classList.remove("show-menu");
+  navMenu.classList.remove('show-menu');
 };
 
-navLink.forEach((n) => n.addEventListener("click", handlerLinkAction));
+navLink.forEach((n) => n.addEventListener('click', handlerLinkAction));
 /*====================  SKILLS ====================*/
 
-const skillsContent = document.getElementsByClassName("skills__content");
-const skillsHeader = document.querySelectorAll(".skills__header");
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
 
 //toggleSkills function
 const toggleSkills = function () {
   let itemClass = this.parentNode.className;
 
   for (let i = 0; i < skillsContent.length; i++) {
-    skillsContent[i].className = "skills__content skills__close";
+    skillsContent[i].className = 'skills__content skills__close';
   }
 
-  if (itemClass === "skills__content skills__close") {
-    this.parentNode.className = "skills__content skills__open";
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open';
   }
 };
 
 skillsHeader.forEach((el) => {
-  el.addEventListener("click", toggleSkills);
+  el.addEventListener('click', toggleSkills);
 });
 
 //toggle skills: TODO: fix BUG:
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll("[data-target");
-const tabContents = document.querySelectorAll("[data-content]");
+// const tabs = document.querySelectorAll("[data-target");
+// const tabContents = document.querySelectorAll("[data-content]");
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.target);
+// tabs.forEach((tab) => {
+//   tab.addEventListener("click", () => {
+//     const target = document.querySelector(tab.dataset.target);
 
-    tabContents.forEach((tabContent) => {
-      tabContent.classList.remove("qualification__active");
-    });
-    target.classList.add("qualification__active");
+//     tabContents.forEach((tabContent) => {
+//       tabContent.classList.remove("qualification__active");
+//     });
+//     target.classList.add("qualification__active");
 
-    tabs.forEach((tab) => {
-      tab.classList.remove("qualification__active");
-    });
-    tab.classList.add("qualification__active");
-  });
-});
+//     tabs.forEach((tab) => {
+//       tab.classList.remove("qualification__active");
+//     });
+//     tab.classList.add("qualification__active");
+//   });
+// });
 
 /*==================== SERVICES MODAL ====================*/
 
-const modalViews = document.querySelectorAll(".services__modal");
-const modalBtns = document.querySelectorAll(".services__button");
-const modalCloses = document.querySelectorAll(".services__modal-close");
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalCloses = document.querySelectorAll('.services__modal-close');
 
 console.log(modalBtns);
 const modalHandler = function (modalOpens) {
-  modalViews[modalOpens].classList.add("active-modal");
+  modalViews[modalOpens].classList.add('active-modal');
 };
 
 modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener("click", (e) => {
+  modalBtn.addEventListener('click', (e) => {
     console.log(e);
     modalHandler(i);
   });
 });
 
 modalCloses.forEach((modalClose) => {
-  modalClose.addEventListener("click", () => {
+  modalClose.addEventListener('click', () => {
     modalViews.forEach((modalView) => {
-      modalView.classList.remove("active-modal");
+      modalView.classList.remove('active-modal');
     });
   });
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const scrollBtn = document.getElementById("scroll-up");
+const scrollBtn = document.getElementById('scroll-up');
 
 // When the user scrolls down x px from the top of the document, show the button
 window.onscroll = function () {
@@ -112,9 +112,9 @@ const scrollFunction = function () {
     document.body.scrollTop > 800 ||
     document.documentElement.scrollTop > 800
   ) {
-    scrollBtn.style.display = "block";
+    scrollBtn.style.display = 'block';
   } else {
-    scrollBtn.style.display = "none";
+    scrollBtn.style.display = 'none';
   }
 };
 
@@ -124,40 +124,40 @@ const topFunction = function () {
   document.documentElement.scrollTop = 0;
 };
 
-scrollBtn.addEventListener("click", topFunction);
+scrollBtn.addEventListener('click', topFunction);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
     darkTheme
   );
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
     iconTheme
   );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
+themeButton.addEventListener('click', () => {
   // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
   // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
 });
